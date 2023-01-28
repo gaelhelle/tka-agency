a7cb684f253b53b3c15d9b246bad92bdc8b2c466
Gael (perso) <me@gaelhelle.fr>
2023-01-27 21:57:30 -0800

    first commit

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "brand-bg": "#212121",
+        "brand-primary": " #ffea28",
+        "brand-text": "#ffffff",
+        "brand-text-secondary": "#acacac",
+      },
+    },
+  },
+};
+
+AOS.init();
