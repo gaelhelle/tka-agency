7236fcf99c2e2f3234135d4d0414f0fe4d6997eb
Gael (perso) <me@gaelhelle.fr>
2023-01-28 20:28:55 -0800

    feat: enhanced talent img quality and effect on mobile

--- assets/script.js
+++ assets/script.js
@@ -25,3 +25,28 @@ for (const button of toggleButtons) {
   };
   button.addEventListener("click", handleCollapse);
 }
+
+// Animate player pictures
+// Find the item we want to animate on scroll
+var talents = document.querySelectorAll(".talent-item");
+var targetActiveClass = "is-active";
+
+// Call this function when it enters/leaves the viewport
+var callback = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add(targetActiveClass);
+    } else {
+      entry.target.classList.remove(targetActiveClass);
+    }
+  });
+};
+
+var observer = new IntersectionObserver(callback, {
+  threshold: 0,
+  rootMargin: "-120px",
+});
+
+for (const talent of talents) {
+  observer.observe(talent);
+}
